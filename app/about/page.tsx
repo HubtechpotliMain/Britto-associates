8157352f5eb9a97a7b0495775aaa88ba8ebfbdae
YASH2204V <yash.v220104@gmail.com>
2026-02-10 14:08:12 +0530

    feat: founder image fix in /about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -178,7 +178,7 @@ export default function About() {
           <div className={styles.founderContent}>
             <div className={styles.founderImageWrapper}>
               <img
-                src="/owner photo/image.png"
+                src="/founder/6.jpeg"
                 alt="Mr. Antony John Britto - Founder of Britto Associates"
                 className={styles.founderImage}
               />
